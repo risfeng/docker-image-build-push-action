ccd9bc549bd7b7df39abd1035fd2b23d922e75de
risfeng <risfeng@126.com>
2020-03-24 16:19:19 +0800

    build and push docker image to aliyun hub fix.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM alpine:latest
+RUN apk add docker \
+    && service docker start \
+    && docker -v
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -l
 echo "login docker start..."
-echo $INPUT_PASSWORD | sudo docker login --username=$INPUT_USER_NAME $INPUT_REGISTRY_URL --password-stdin
+echo $INPUT_PASSWORD | docker login --username=$INPUT_USER_NAME $INPUT_REGISTRY_URL --password-stdin
 echo "docker build start..."
-sudo docker_file=$INPUT_DOCKER_FILE
+docker_file=$INPUT_DOCKER_FILE
 if [ "${docker_file}" = "" ]; then
     docker_file="."
 fi
-sudo docker build -t $INPUT_IMAGE_NAME:$INPUT_IMAGE_VERSION ${docker_file}
+docker build -t $INPUT_IMAGE_NAME:$INPUT_IMAGE_VERSION ${docker_file}
 echo "docker tag start..."
-sudo docker tag $INPUT_IMAGE_NAME:$INPUT_IMAGE_VERSION $INPUT_REGISTRY_URL/$INPUT_NAMESPACES/$INPUT_REPOSITORY_NAME:$INPUT_IMAGE_VERSION
+docker tag $INPUT_IMAGE_NAME:$INPUT_IMAGE_VERSION $INPUT_REGISTRY_URL/$INPUT_NAMESPACES/$INPUT_REPOSITORY_NAME:$INPUT_IMAGE_VERSION
 echo "docker push start..."
-sudo docker push $INPUT_REGISTRY_URL/$INPUT_NAMESPACES/$INPUT_REPOSITORY_NAME:$INPUT_IMAGE_VERSION
+docker push $INPUT_REGISTRY_URL/$INPUT_NAMESPACES/$INPUT_REPOSITORY_NAME:$INPUT_IMAGE_VERSION
 echo "build build and push success."
 echo "docker logout..."
-sudo docker logout
+docker logout
 echo "参数： $INPUT_USER_NAME  ${docker_file}  $INPUT_REGISTRY_URL  $INPUT_IMAGE_NAME:$INPUT_IMAGE_VERSION $INPUT_REGISTRY_URL/$INPUT_NAMESPACES/$INPUT_REPOSITORY_NAME:$INPUT_IMAGE_VERSION"
